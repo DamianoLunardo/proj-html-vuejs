15f25ba5c8d5f3f00996f4696a8eabd3f932b5d9
DamianoL <damianolunardo@hotmail.it>
2023-11-15 11:16:55 +0100

    Create project folders, basic folder structure

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    
+  },
+  data () {
+    return {
+      message: 'Ciao',
+    
+    }
+  }
+}
+
+
+</script>
+
+<template>
+  <AppHeader />
+ 
+  </template>
+  
+
+<style lang="scss" scoped>
+
+</style>
